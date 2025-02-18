1f06f7521a27091139526b905032e9362477dce8
jutanke-sony <julian.tanke@sony.com>
2025-02-18 11:46:18 -0500

    better vis

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="humanposer",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=["matplotlib", "numpy", "tqdm", "einops", "smplx", "chumpy"],
     python_requires=">=3.8",
