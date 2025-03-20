858f822023d0c57f2106e8e3685b084f539551a7
jutanke-sony <julian.tanke@sony.com>
2025-03-20 16:16:07 -0400

    add smplx names

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="humanposer",
-    version="1.0.7",
+    version="1.0.8",
     packages=find_packages(),
     install_requires=["matplotlib", "numpy", "tqdm", "einops", "gdown"],
     python_requires=">=3.8",
